ca42b94f8f481a7fe76e35edaa07ad553f054598
raul-ae <raul.ae@hotmail.com>
2020-07-09 16:39:56 -0500

    adding all functionalities

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,58 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-eaten").on("click", function(event) {
+      var id = $(this).data("id");
+      var newEaten = $(this).data("neweaten");
+  
+      var newEatenState = {
+        eaten: newEaten
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newEatenState
+      }).then(
+        function() {
+          console.log("changed burger to", newEaten);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+  $(".delete-burger").on("click", function(event){
+    var id = $(this).data("id");
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function(){
+        console.log("deleted burger", id);
+        location.reload();
+      }
+    );
+  });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#ca").val().trim(),
+        eaten: "0"
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
